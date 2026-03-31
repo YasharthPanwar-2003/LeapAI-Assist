809afd6b8d34d77bd02b223a7b5462e7388a3280
YasharthPanwar-2003 <panwaryasharth203@gmail.com>
2026-03-31 10:14:11 +0530

    feat: OS integration module exports (version detector + system context)
    Signed-off-by: YasharthPanwar-2003 <panwaryasharth203@gmail.com>

---
+++ src/os_integration/__init__.py
@@ -0,0 +1,9 @@
+from src.ingestion.zypper_extractor import ZypperExtractor
+from src.os_integration.system_context import SystemContext
+from src.os_integration.version_detector import VersionDetector
+
+__all__ = [
+    "VersionDetector",
+    "SystemContext",
+    "ZypperExtractor",
+]
